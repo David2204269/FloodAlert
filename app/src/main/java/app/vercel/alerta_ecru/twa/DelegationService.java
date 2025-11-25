408e965aa7052010384f1dcf0aa17da1ca57c0c3
Luis Hernandez <hernandezgalvis830@gmail.com>
2025-11-25 09:52:30 -0500

    cosas de graddle para la aplicacion movil

---
+++ app/src/main/java/app/vercel/alerta_ecru/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.alerta_ecru.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
